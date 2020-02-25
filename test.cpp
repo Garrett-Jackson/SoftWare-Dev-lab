21018d38cd119f7e9f05df9ba28f1c7fd6ad49e5
Garrett-Jackson <37818388+Garrett-Jackson@users.noreply.github.com>
2020-02-25 10:34:48 -0600

    added a meth function
    specifically squares and prints junk

--- test.cpp
+++ test.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
 using namespace std;
+void sqr(int num);
+
 
 int main()
 {
 
     cout<<" Git Test  "<<endl;
 
+    sqr(5);
     return 0;
+}
+//--
+
+void sqr(int num)
+{
+    int result = num *num;
+    cout <<result <<endl;
+
 }
