873f5137975aa4237b3fb86299f2335490dab6b9
baileywellen <baileywellen@gmail.com>
2020-02-25 11:08:27 -0600

    Allow doubles for base in power() function.
    We can now input doubles for the base of the power function.

--- test.cpp
+++ test.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 
 using namespace std;
-void sqr(int num);
-
+void power(double base, int pwr);
 
 int main()
 {
 
     cout<<" Git Test  "<<endl;
 
-    sqr(5,2);
+    power(5.0,2);
+	system("pause");
     return 0;
 }
 //--
 
-void sqr(int num, int pwr)
+void power(double base, int pwr)
 {
     int result= 1;
     for(int i =0; i < pwr; i++)
     {
-      result   =  num * result;
+      result   =  base * result;
     }
   
     cout <<result <<endl;
